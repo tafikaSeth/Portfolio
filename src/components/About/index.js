4ec81508bdb2369ccf5c80797bc833855e802713
tafikaSeth <tafikaseth@gmail.com>
2025-01-28 08:39:09 +0100

    "responsive"

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -26,13 +26,13 @@ const About = () => {
                             />
                         </div>
                         <motion.p
-                                initial = {{ opacity:0, x: 50 }}
-                                whileInView={{ opacity:1, x: 0, transition: {delay: 0.2, duration: 0.5} }}
+                                initial = {{ opacity:0, y:50 }}
+                                whileInView={{ opacity:1, y:0, transition: {delay: 0.2, duration: 0.5} }}
                                 viewport={{ once: false, amount: .5 }}
                         >My name's TAFIKA Aintsoa Sarobidy Seth but everybody calls me Tafika or Seth, i've 23 years old and i live in Fianarantsoa Madagascar.I'm currently doing a Master 1 in Computer Science at University of EMIT Fianarantsoa(Ecole de Management et d'Innovation Technologique), passionate about web development with a solid academic background and practical experience in various projects.I have acquired in-depth skills in front-end development such as ReactJs and back-end development in laravel.My objectif is to create innovative, high-performance web solutions.</motion.p>
                         <motion.p
-                                initial = {{ opacity:0, x: 50 }}
-                                whileInView={{ opacity:1, x: 0, transition: {delay: 0.2, duration: 0.5} }}
+                                initial = {{ opacity:0, y:50 }}
+                                whileInView={{ opacity:1, y:0, transition: {delay: 0.2, duration: 0.5} }}
                                 viewport={{ once: false, amount: .5 }}
                         >Curious and always on the lookout for new knowledge.I strive to keep up to date with the latest technologies and industry trends.</motion.p>
                     </div>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import Menu from "../Assets/icons8-menu-50.png"
 import pathFb from '../Assets/Icons/fb (2).png'
