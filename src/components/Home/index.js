b96994e28716ffe22415236eb395122a28c4d311
tafikaSeth <tafikaseth@gmail.com>
2025-02-11 20:58:42 +0100

    "home: 320px"

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -34,6 +34,7 @@ const Home = () => {
     return(
         <div id="home" className="container-home">
             <div className="content-home">
+                <div className="content-home-image"></div>
                 <div className="content-home-name">
                     <h1>Hello,<br/>I'm <span className="word-color">{displayText}</span> <br/><span className="developer">Web Developer & mobile</span></h1>
                     <div className="btn-contact">
@@ -49,7 +50,6 @@ const Home = () => {
                         </motion.button>
                     </div>
                 </div>
-                <div className="content-home-image"></div>
             </div>
         </div>
     )
