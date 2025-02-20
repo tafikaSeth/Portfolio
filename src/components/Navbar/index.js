380c140b0d3247e0f4ae4405a49c954f75124e24
tafikaSeth <tafikaseth@gmail.com>
2025-02-20 09:09:40 +0100

    "feat: add favicon, href SEO"

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -39,8 +39,8 @@ const Navbar = () => {
             </ul>
             <div className="navbar-mode">
                 <a href="https://web.facebook.com/profile.php?id=100092466422462" target='_blank' rel='noopener noreferrer'><img src={pathFb} alt="facebook"/></a>
-                <a href="#linkedin"><img src={pathIn} alt="linkedin"/></a>
-                <a href="#whatsapp"><img src={pathApp} alt="whatsapp"/></a>
+                <a href="https://web.facebook.com/profile.php?id=100092466422462"><img src={pathIn} alt="linkedin"/></a>
+                <a href="https://web.facebook.com/profile.php?id=100092466422462"><img src={pathApp} alt="whatsapp"/></a>
             </div>
         </nav>
     )
