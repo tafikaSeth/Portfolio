6efc4d2bb1189521ec9269a6c98f3b9d9338eb61
tafikaSeth <tafikaseth@gmail.com>
2025-02-20 08:30:19 +0100

    "feat: add alt to image"

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -29,7 +29,7 @@ const Navbar = () => {
                     </div>
                 </div>
             <div className="navbar-logo">SETH</div>
-            <img onClick={openMenuMobile} className="menu" src={Menu}/>
+            <img onClick={openMenuMobile} className="menu" src={Menu} alt="Menu"/>
             <ul className="navbar-links">
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
