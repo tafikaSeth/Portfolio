725de14a44315d3051d8e8d68b9f1c213dcf3eae
tafikaSeth <tafikaseth@gmail.com>
2025-02-25 13:42:10 +0100

    "feat: addin loading page"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./App.css"
 import Home from './components/Home';
 import About from './components/About';
@@ -7,8 +7,31 @@ import Skills from './components/Skills';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 import Ribbons from "./components/Animation"
+import Ballpit from './components/Loading';
+import { useState } from 'react';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 5000)
+  }, [])
+
+  if(isLoading){
+    return(
+      <div style={{position: 'relative', overflow: 'hidden', height: '100vh', width: '100%', backgroundColor: '#212121'}}>
+        <Ballpit
+          count={100}
+          colors= {['white','green','#14FF00']}
+          gravity={0.5}
+          friction={0.8}
+          wallBounce={0.95}
+          followCursor={true}
+        />
+      </div>
+    )
+  }
   return (
     <div className='App'>
       <Navbar/>
