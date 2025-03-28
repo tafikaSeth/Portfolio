14a945ae2e6ae7a5febf52d56610f5d5b8941e0d
tafikaSeth <tafikaseth@gmail.com>
2025-03-28 04:07:01 +0100

    refact: remove the component loading in App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
 import "./App.css"
 import Home from './components/Home';
 import About from './components/About';
@@ -7,35 +7,35 @@ import Skills from './components/Skills';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 import Ribbons from "./components/Animation"
-import Ballpit from './components/Loading';
+// import Ballpit from './components/Loading';
 import { useState } from 'react';
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(true)
+  // const [isLoading, setIsLoading] = useState(true)
 
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false)
-    }, 10000)
-  }, [])
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setIsLoading(false)
+  //   }, 10000)
+  // }, [])
 
-  if(isLoading){
-    return(
-      <div style={{background: 'red',position: 'relative', overflow: 'hidden', height: '100vh', width: '100%', backgroundColor: '#212121'}}>
-        <Ballpit
-          // count={100}
-          count={50}
-          colors= {['white','red','#14FF00']}
-          gravity={0.5}
-          friction={0.889}
-          wallBounce={0.95}
-          followCursor={true}
-          maxZ= {1}
-        />
-        <h1 style={{fontFamily: 'Lexend,sans-serif', color: '#fff', fontSize: '50px',textAlign: 'center',marginTop: '-25rem'}}>--LOADING--</h1>
-      </div>
-    )
-  }
+  // if(isLoading){
+  //   return(
+  //     <div style={{background: 'red',position: 'relative', overflow: 'hidden', height: '100vh', width: '100%', backgroundColor: '#212121'}}>
+  //       <Ballpit
+  //         // count={100}
+  //         count={50}
+  //         colors= {['white','red','#14FF00']}
+  //         gravity={0.5}
+  //         friction={0.889}
+  //         wallBounce={0.95}
+  //         followCursor={true}
+  //         maxZ= {1}
+  //       />
+  //       <h1 style={{fontFamily: 'Lexend,sans-serif', color: '#fff', fontSize: '50px',textAlign: 'center',marginTop: '-25rem'}}>--LOADING--</h1>
+  //     </div>
+  //   )
+  // }
   return (
     <div className='App'>
       <Navbar/>
